a92d94e12d971e6f4c987bc1fc8edf964550b2fd
Ahreum Hong <ahreumhong00@gmail.com>
2026-07-30 09:59:36 +0900

    feat(body-q): redesign index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,16 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
+import './styles.css';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find the root element.');
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
